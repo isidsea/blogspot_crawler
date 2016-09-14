3d0055c91cf1079f0a71faf8ce781607eb996adb
Frans <franssiswanto@gmail.com>
2016-09-14 13:15:23 +0700

    multithreading migration

--- lib/validator/response.py
+++ lib/validator/response.py
@@ -12,6 +12,6 @@ class ResponseValidator(Validator):
 			if response["error"]["code"] == 404: raise CannotFindBlog("Cannot find the blog inside blogspot")
 			if response["error"]["code"] == 503: raise APIBackendError("Google Blogspot API Backend error 503")
 			if response["error"]["code"] == 403: raise APIKeyLimitExceed("Running out of request limit.")
-			raise CannotFindBlog("Cannot find the blog inside blogspot with error: %s" % response["code"])
+			raise CannotFindBlog("Cannot find the blog inside blogspot with error: %s" % response)
 		elif "items" not in response:
 			raise EmptyPost("Blog does not have any post.")
--- migrate.py
+++ migrate.py
@@ -7,10 +7,40 @@ from lib.network_tools import NetworkTools
 from lib.database      import Database
 from lib.api.factory   import APIFactory
 from curtsies          import fmtstr
+from multiprocessing   import Pool
 import os
 import requests
 import pymongo
 import arrow
+import tomorrow
+
+@tomorrow.threads(10)
+def execute_worker(args):
+	try:
+		url, country = args
+		print("[migrate][debug] URL: %s" % url.encode("utf8"))
+		get_by_url = APIFactory.get_api(APIFactory.GET_BY_URL)
+		res        = get_by_url.execute(url=url)
+
+		if country is None: raise CannotFindBlog("Country is null")
+		document = {
+			          "id" : res["id"],
+			        "name" : res["name"],
+			 "description" : res["description"],
+			     "created" : arrow.get(res["published"]).datetime,
+			     "updated" : arrow.get(res["updated"]).datetime,
+			         "url" : res["url"],
+			      "domain" : NetworkTools.get_domain(res["url"], with_scheme=False),
+			     "country" : country,
+		    "_insert_time" : arrow.utcnow().datetime,
+		       "is_active" : True
+		}
+		db = Database.get_db()
+		db.blog_list.insert_one(document)
+	except CannotFindBlog as ex:
+		print(fmtstr("[migrate][debug] %s" % ex,"red"))
+	except pymongo.errors.DuplicateKeyError:
+		print(fmtstr("[migrate][debug] Blog has already exists.","red"))
 
 if __name__ == "__main__":
 	file = os.path.join(os.getcwd(),"data","blog_list.csv")
@@ -18,42 +48,19 @@ if __name__ == "__main__":
 
 	print("[migrate][debug] Migarting...")
 	file = open(file,"r", encoding="utf-8")
+	data = []
 	for line in file.readlines():
-		try:
-			line    = line.replace("\n","")
-			line    = line.split(",")
-			country = line[0]
-			url     = line[2]
-			url     = url.replace('"',"")
-			url     = url.replace("'","")
-			url     = url.replace(" ","")
-			url     = url.replace("#","")
-			url     = url.replace("-","")
-			url     = url.replace("%","")
-			url     = NetworkTools.full_url(url)
-			# basic_url = url.replace("\n","")
-
-			print("[migrate][debug] URL: %s" % url.encode("utf8"))
-			get_by_url = APIFactory.get_api(APIFactory.GET_BY_URL)
-			res        = get_by_url.execute(url=url)
-
-			if country is None: raise CannotFindBlog("Country is null")
-			document = {
-				          "id" : res["id"],
-				        "name" : res["name"],
-				 "description" : res["description"],
-				     "created" : arrow.get(res["published"]).datetime,
-				     "updated" : arrow.get(res["updated"]).datetime,
-				         "url" : res["url"],
-				      "domain" : NetworkTools.get_domain(res["url"], with_scheme=False),
-				     "country" : country,
-			    "_insert_time" : arrow.utcnow().datetime,
-			       "is_active" : True
-			}
-			db = Database.get_db()
-			db.blog_list.insert_one(document)
-		except CannotFindBlog as ex:
-			print(fmtstr("[migrate][debug] %s" % ex,"red"))
-		except pymongo.errors.DuplicateKeyError:
-			print(fmtstr("[migrate][debug] Blog has already exists.","red"))
+		line    = line.replace("\n","")
+		line    = line.split(",")
+		country = line[0]
+		url     = line[2]
+		url     = url.replace('"',"")
+		url     = url.replace("'","")
+		url     = url.replace(" ","")
+		url     = url.replace("#","")
+		url     = url.replace("-","")
+		url     = url.replace("%","")
+		url     = NetworkTools.full_url(url)
+		data.append((url, country))
+	result = [execute_worker(datum) for datum in data]
 	print("[migrate][debug] Migrated!")
